1bb913f36631d1ee333ce01a3617ca7541e0e620
muxmonster <muxmonster@gmail.com>
2021-09-14 21:56:26 +0700

    begin

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      builder: (context, widget) => ResponsiveWrapper.builder(
+          BouncingScrollWrapper.builder(context, widget!),
+          maxWidth: 1200,
+          minWidth: 450,
+          defaultScale: true,
+          breakpoints: [
+            ResponsiveBreakpoint.resize(450, name: MOBILE),
+            ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+            ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+            ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+          ],
+          background: Container(color: Color(0xFFF5F5F5))),
+      initialRoute: Routes.home,
+      onGenerateRoute: (RouteSettings settings) {
+        return Routes.fadeThrough(settings, (context) {
+          switch (settings.name) {
+            case Routes.home:
+              return ListPage();
+            default:
+              return SizedBox.shrink();
+          }
+        });
+      },
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
